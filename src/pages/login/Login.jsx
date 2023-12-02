8921ee0c681eceba5b87a8c352d4b8005a5a3bb6
christian <ronquechristian@gmail.com>
2023-12-03 00:58:14 +0800

    final login func

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import './Login.scss';
-import axios from 'axios';
+import newRequest from '../../utils/newRequest';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(
-        'http://localhost:8800/api/auth/login',
-        {
-          username,
-          password,
-        }
-      );
-      console.log(res.data);
+      const res = await newRequest.post('/auth/login', {
+        username,
+        password,
+      });
+      localStorage.setItem('currentUser', JSON.stringify(res.data));
+      navigate('/');
     } catch (err) {
-      setError(err);
-      console.log(err);
+      setError(err.response.data);
     }
   };
 
@@ -43,6 +43,7 @@ export const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
+        {error && error}
       </form>
     </div>
   );
