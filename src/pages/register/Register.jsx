ef903932b27d25a3df5b592b9b10718e3c71af49
Janeo Benidi <benidi.janeo@gmail.com>
2024-06-14 02:48:30 +0800

    change button color

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
-import upload from '../../utils/upload';
-import './Register.scss';
-import newRequest from '../../utils/newRequest';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import upload from "../../utils/upload";
+import "./Register.scss";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Register = () => {
   const [file, setFile] = useState(null);
   const [resumeFile, setResumeFile] = useState(null);
   const [validIDFile, setValidIDFile] = useState(null);
-  const [user, setUser] = useState({
-    username: '',
-    email: '',
-    password: '',
-    img: '',
-    country: '',
-    isSeller: false,
-    desc: '',
-  });
+  const [user, setUser] = useState({});
+
+  console.log(user);
 
   const navigate = useNavigate();
 
@@ -35,13 +29,13 @@ export const Register = () => {
 
   const uploadPDF = async (file) => {
     const data = new FormData();
-    data.append('file', file);
-    data.append('upload_preset', 'eduScape');
-    data.append('resource_type', 'raw');
+    data.append("file", file);
+    data.append("upload_preset", "eduScape");
+    data.append("resource_type", "raw");
 
     try {
       const res = await axios.post(
-        'https://api.cloudinary.com/v1_1/eduscape/raw/upload',
+        "https://api.cloudinary.com/v1_1/eduscape/raw/upload",
         data
       );
 
@@ -59,13 +53,13 @@ export const Register = () => {
     const urlResume = await uploadPDF(resumeFile);
     const urlValidID = await upload(validIDFile);
     try {
-      await newRequest.post('/auth/register', {
+      await newRequest.post("/auth/register", {
         ...user,
         img: url,
         resume: urlResume,
         validID: urlValidID,
       });
-      navigate('/');
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
@@ -99,16 +93,9 @@ export const Register = () => {
             onChange={handleChange}
           />
           <label htmlFor="">Password</label>
-          <input
-            name="password"
-            type="password"
-            onChange={handleChange}
-          />
+          <input name="password" type="password" onChange={handleChange} />
           <label htmlFor="">Profile Picture</label>
-          <input
-            type="file"
-            onChange={(e) => setFile(e.target.files[0])}
-          />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
           <label htmlFor="">Province</label>
           <input
             name="province"
