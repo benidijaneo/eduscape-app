8a86aa844eb96f5c0da98208b18e1bb1e798cb32
Janeo Benidi <benidi.janeo@gmail.com>
2024-06-23 15:45:06 +0800

    API integration for the contact and user cv

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './GigCard.scss';
-import { useQuery } from '@tanstack/react-query';
-import newRequest from '../../utils/newRequest';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./GigCard.scss";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import DescriptionShortener from "../utilComponents/descriptionShortener";
 
 export const GigCard = ({ item }) => {
   const { isLoading, error, data } = useQuery({
-    queryKey: ['gigUser', item.userId],
+    queryKey: ["gigUser", item.userId],
     queryFn: async () => {
       const res = await newRequest.get(`/users/${item.userId}`);
-      console.log('API Response:', res.data);
+      console.log("API Response:", res.data);
       return res.data;
     },
   });
@@ -20,21 +21,23 @@ export const GigCard = ({ item }) => {
         <img src={item.cover} alt="" />
         <div className="info">
           {isLoading ? (
-            'loading'
+            "loading"
           ) : error ? (
-            'error'
+            "error"
           ) : (
             <div className="user">
-              <img src={data.img || '/img/noavatar.jpg'} alt="" />
+              <img src={data.img || "/img/noavatar.jpg"} alt="" />
               <span>{`${data.firstName} ${data.lastName}`}</span>
             </div>
           )}
-          <p>{item.desc}</p>
+          <p>
+            <DescriptionShortener description={item.desc} />
+          </p>
           <div className="star">
             <img src="./img/star.png" alt="" />
             <span>
               {isNaN(Math.round(item.totalStars / item.startNumber))
-                ? ''
+                ? ""
                 : Math.round(item.totalStars / item.startNumber)}
             </span>
           </div>
